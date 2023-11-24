8e3f564078d4bbecc8791cc79ba012b095cb357e
Henrique Nascimento <aluno24173@ipt.pt>
2023-11-24 22:37:07 +0000

    activity login para activity sign_in e vice versa

--- app/src/main/java/pt/ipt/dam2023/cartrade/SignIn.kt
+++ app/src/main/java/pt/ipt/dam2023/cartrade/SignIn.kt
@@ -1,12 +1,20 @@
 package pt.ipt.dam2023.cartrade
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignIn: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
+
+        findViewById<Button>(R.id.btnToLogin).setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
 }
