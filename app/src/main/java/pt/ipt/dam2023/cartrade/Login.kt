3afdf4543be72c8113aa462b46ab4caa8e9bc29b
Henrique Nascimento <aluno24173@ipt.pt>
2023-11-27 15:12:47 +0000

    login

---
+++ app/src/main/java/pt/ipt/dam2023/cartrade/Login.kt
@@ -0,0 +1,21 @@
+package pt.ipt.dam2023.cartrade
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Login: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        findViewById<TextView>(R.id.txtToSignIn).setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+    }
+}
