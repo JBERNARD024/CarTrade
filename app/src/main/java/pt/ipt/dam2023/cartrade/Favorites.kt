e638bc7124c0b50df8c16983f5943a84aae90697
JBERNARD024 <joaobernardo.v@gmail.com>
2024-01-05 02:50:44 +0000

    Configuração da navigation bar

---
+++ app/src/main/java/pt/ipt/dam2023/cartrade/Favorites.kt
@@ -0,0 +1,12 @@
+package pt.ipt.dam2023.cartrade
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Favorites: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favorites)
+    }
+}
--- app/src/main/java/pt/ipt/dam2023/cartrade/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2023/cartrade/MainActivity.kt
@@ -1,41 +1,60 @@
 package pt.ipt.dam2023.cartrade
 
 import android.content.Intent
+import android.net.http.HeaderBlock
 import android.os.Bundle
+import android.text.Layout
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configurar a barra de ação (ou Toolbar)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout);
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val headerView = navigationView.getHeaderView(0)
+
+        headerView.setOnClickListener { val intent = Intent(this, Login::class.java)
+            startActivity(intent) }
+
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if(savedInstanceState == null){
+            navigationView.setCheckedItem(R.id.nav_view)
+        }
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflar o menu na barra de ação
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Manipular os itens do menu aqui
-        when (item.itemId) {
-            R.id.action_login -> {
-                // Exemplo: Iniciar uma nova atividade quando o item Settings é clicado
-                val intent = Intent(this, Login::class.java)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.favorite -> {
+                val intent = Intent(this, Favorites::class.java)
                 startActivity(intent)
                 return true
             }
-            R.id.action_registar -> {
-                // Exemplo: Iniciar uma nova atividade quando o item Settings é clicado
-                val intent = Intent(this, SignIn::class.java)
+            R.id.info -> {val intent = Intent(this, Favorites::class.java)
                 startActivity(intent)
                 return true
             }
