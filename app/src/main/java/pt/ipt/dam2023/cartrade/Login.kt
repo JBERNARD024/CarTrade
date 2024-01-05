63e2fc197de8bd8b766430ed2403dd5b369097b7
JBERNARD024 <joaobernardo.v@gmail.com>
2024-01-05 05:58:00 +0000

    Adição da Base de Dados Configuração do Registo e da Autenticação.

--- app/src/main/java/pt/ipt/dam2023/cartrade/Login.kt
+++ app/src/main/java/pt/ipt/dam2023/cartrade/Login.kt
@@ -2,15 +2,25 @@ package pt.ipt.dam2023.cartrade
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
 
 
 class Login: AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var db: DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
@@ -19,6 +29,29 @@ class Login: AppCompatActivity() {
             val intent = Intent(this, SignIn::class.java)
             startActivity(intent)
         }
-    }
 
+        email = findViewById(R.id.txtEmail)
+        password = findViewById(R.id.txtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        db = DBHelper(this)
+        
+        btnLogin.setOnClickListener { 
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
+
+            if(TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText)){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }else{
+                val verificaUser = db.verificaUtilizador(emailText, passwordText)
+                if(verificaUser==true){
+                    Toast.makeText(this, "Autenticação bem sucedida", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "Dados Inválidos", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+
+    }
 }
--- app/src/main/java/pt/ipt/dam2023/cartrade/SignIn.kt
+++ app/src/main/java/pt/ipt/dam2023/cartrade/SignIn.kt
@@ -2,10 +2,21 @@ package pt.ipt.dam2023.cartrade
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SignIn: AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var confPassword: EditText
+    private lateinit var btnCriarConta: Button
+    private lateinit var db:DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
@@ -15,6 +26,38 @@ class SignIn: AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        username = findViewById(R.id.txtUsername)
+        email = findViewById(R.id.txtEmail)
+        password = findViewById(R.id.txtPassword)
+        confPassword = findViewById(R.id.txtConfirmarPassword)
+        btnCriarConta = findViewById(R.id.btnCriarConta)
+        db = DBHelper(this)
+
+        btnCriarConta.setOnClickListener {
+            val userText = username.text.toString()
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
+            val confPasswordText = confPassword.text.toString()
+            val guardarDados = db.inserirDados(userText, emailText, passwordText)
+
+            if(TextUtils.isEmpty(userText) || TextUtils.isEmpty(emailText) || TextUtils.isEmpty(passwordText) || TextUtils.isEmpty(confPasswordText)){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }else{
+                if(passwordText.equals(confPasswordText)){
+                    if(guardarDados==true){
+                        Toast.makeText(this, "Registo concluído com sucesso", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "Utilizador já registado", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, Login::class.java)
+                        startActivity(intent)
+                    }
+                }else{
+                    Toast.makeText(this, "Passwords não coincidem", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        
     }
 
 }
